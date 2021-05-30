a89f1ddede67c06631bfa454e3cd0bcb5ae3acb0
Fliderman01 <Fliderman01@gmail.com>
2021-05-30 08:51:06 +0400

    completed

--- main.js
+++ main.js
@@ -1,19 +1,20 @@
-// const left = document.querySelector('#left'); // მარცხენა ღილაკი
-// const right = document.querySelector('#right'); // მარჯვენა ღილაკი
-
-// // მარცხენა ღილაკი
-//   left.addEventListener('click', (e) => {
-//     document.querySelector('.span1').style.display = 'initial'; // ტანიას პარაგრაფის გაჩენა
-//     document.querySelector('.span2').style.display = 'none'; // ჯონის პარაგრაფის გაქრობა
-//   });
-
-// // მარჯვენა ღილაკი
-// right.addEventListener('click', (e) => {
-//     document.querySelector('.span1').style.display = 'none'; // ტანიას პარაგრაფის გაქრობა
-//     document.querySelector('.span2').style.display = 'initial'; // ჯონის პარაგრაფის გაჩენა
-//   });
-
 function myFunction() {
-    document.querySelectorAll('.span1').style.display = 'none'; // ტანიას პარაგრაფის გაჩენა
-    document.querySelector('.span2').style.display = 'initial'; // ჯონის პარაგრაფის გაქრობა
-  };
+  const a = document.getElementById("199");
+  if (a.innerHTML === "19.99") {
+    a.innerHTML = "199.99";
+  } else {
+    a.innerHTML = "19.99";
+  }
+  const b = document.getElementById("249");
+  if (b.innerHTML === "24.99") {
+    b.innerHTML = "249.99";
+  } else {
+    b.innerHTML = "24.99";
+  }
+  const c = document.getElementById("399");
+  if (c.innerHTML === "39.99") {
+    c.innerHTML = "399.99";
+  } else {
+    c.innerHTML = "39.99";
+  }
+}
